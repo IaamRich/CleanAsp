6184393b3250bae0173bac354a337ee37373e4e3
Vladimir Curaciov <vladimircuraciov@gmail.com>
2021-09-16 14:44:40 +0300

    finished lesson 8

--- CleanWebApp/CleanRazonUI/Controllers/HomeController.cs
+++ CleanWebApp/CleanRazonUI/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using CleanRazonUI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CleanRazonUI.Controllers
 {
@@ -35,25 +36,49 @@ namespace CleanRazonUI.Controllers
                 Phone = "+ 3553 2235 23",
                 Address = "2345 Jozeph street 57GH"
             };
+
+            string[] source = { "item 1", "item 2", "item 3" };
+            SelectList selectList = new SelectList(source, source[1]);
+            ViewBag.SelectItems = selectList;
+
             return View(contacts);
+        }
+
+        [HttpPost]
+        public IActionResult Contacts(string selectedItem)
+        {
+            Debug.WriteLine(selectedItem);
+            return View("Result", selectedItem);
         }
 
         public IActionResult SendRequests()
         {
-            return View();
+            return View(new FormViewModel() { Name = "default", TermsAccepted = true });
         }
 
         [HttpPost]
         public IActionResult SendRequests(FormViewModel model, List<string> addresses)
         {
-            Debug.WriteLine(model.Name);
-            Debug.WriteLine(model.Address);
-            Debug.WriteLine(model.Phone);
-            foreach (var item in addresses)
+            //if (string.IsNullOrWhiteSpace(model.Name))
+            //{
+            //    ModelState.AddModelError(nameof(model.Name), "name is empty");
+            //}
+            if (!ModelState.IsValid)
             {
-                Debug.WriteLine(item);
+                Debug.WriteLine("error");
+                return View();
             }
-            return View();
+            else
+            {
+                Debug.WriteLine(model.Name);
+                Debug.WriteLine(model.Address);
+                Debug.WriteLine(model.Phone);
+                foreach (var item in addresses)
+                {
+                    Debug.WriteLine(item);
+                }
+                return View();
+            }
         }
 
         [HttpPost]
--- CleanWebApp/CleanRazonUI/Models/FormViewModel.cs
+++ CleanWebApp/CleanRazonUI/Models/FormViewModel.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace CleanRazonUI.Models
 {
     public class FormViewModel
     {
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; }
         public string Address { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Phone")]
+        [UIHint("Password")] //подсказка
         public string Phone { get; set; }
+        public bool TermsAccepted { get; set; }
     }
 }
